1316b2f11a3ffe2b900557bf7f4d591314386703
samriddhisingh05 <samriddhisingh.cs23@bmsce.ac.in>
2025-03-19 22:14:59 +0530

    SJF_Preemptive.c

--- SJF_Preemptive.c
+++ SJF_Preemptive.c
@@ -1,84 +1,82 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define MAX 10
+void findWaitingTime(int n, int at[], int bt[], int wt[]) {
+    int rt[n];  
+    for (int i = 0; i < n; i++)
+        rt[i] = bt[i];
 
-struct process {
-    int id, AT, BT, CT, TAT, WT, RT, remaining_BT;
-    int completed;
-};
+    int complete = 0, t = 0, min_index, min_rt;
+    int finish_time;
+    int shortest = -1;
+    int check = 0;
 
-void calculate_SJF_Preemptive(struct process p[], int n) {
-    int completed = 0, currentTime = 0;
-    
-    for (int i = 0; i < n; i++) {
-        p[i].remaining_BT = p[i].BT;
-        p[i].RT = -1;
-    }
-
-    while (completed != n) {
-        int shortest = -1, minBT = INT_MAX;
-        
-        for (int i = 0; i < n; i++) {
-            if (!p[i].completed && p[i].AT <= currentTime && p[i].remaining_BT < minBT) {
-                minBT = p[i].remaining_BT;
-                shortest = i;
+    while (complete != n) {
+        min_rt = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            if (at[j] <= t && rt[j] > 0 && rt[j] < min_rt) {
+                min_rt = rt[j];
+                shortest = j;
+                check = 1;
             }
         }
 
-        if (shortest == -1) {
-            currentTime++;
-        } else {
-            if (p[shortest].RT == -1) {  // Set RT only on first execution
-                p[shortest].RT = currentTime - p[shortest].AT;
-            }
-
-            p[shortest].remaining_BT--;
-            currentTime++;
-
-            if (p[shortest].remaining_BT == 0) {
-                p[shortest].CT = currentTime;
-                p[shortest].TAT = p[shortest].CT - p[shortest].AT;
-                p[shortest].WT = p[shortest].TAT - p[shortest].BT;
-                p[shortest].completed = 1;
-                completed++;
-            }
+        if (check == 0) {
+            t++;
+            continue;
         }
+
+        rt[shortest]--;
+        min_rt = rt[shortest];
+
+        if (rt[shortest] == 0) {
+            complete++;
+            finish_time = t + 1;
+            wt[shortest] = finish_time - at[shortest] - bt[shortest];
+
+            if (wt[shortest] < 0)
+                wt[shortest] = 0;
+        }
+        t++;
     }
 }
 
-void display(struct process p[], int n) {
-    float total_tat = 0, total_wt = 0;
+void findTurnAroundTime(int n, int at[], int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
 
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+void findAvgTime(int n, int at[], int bt[]) {
+    int wt[n], tat[n];
+    float total_wt = 0, total_tat = 0;
+
+    findWaitingTime(n, at, bt, wt);
+    findTurnAroundTime(n, at, bt, wt, tat);
+
+    printf("\nProcess\tAT\tBT\tWT\tTAT\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].id, p[i].AT, p[i].BT, p[i].CT, p[i].TAT, p[i].WT, p[i].RT);
-        total_tat += p[i].TAT;
-        total_wt += p[i].WT;
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
     }
 
-    printf("\nAverage TAT: %.2f", total_tat / n);
-    printf("\nAverage WT: %.2f\n", total_wt / n);
+    printf("\nAverage Waiting Time = %.2f", total_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
 }
 
 int main() {
     int n;
-    struct process p[MAX];
-
-    printf("Enter number of processes: ");
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
 
+    int at[n], bt[n];
+    printf("Enter Arrival Time and Burst Time for each process:\n");
     for (int i = 0; i < n; i++) {
-        p[i].id = i + 1;
-        printf("Enter Arrival Time (AT) for process %d: ", i + 1);
-        scanf("%d", &p[i].AT);
-        printf("Enter Burst Time (BT) for process %d: ", i + 1);
-        scanf("%d", &p[i].BT);
-        p[i].completed = 0;
+        printf("Process %d: ", i + 1);
+        scanf("%d %d", &at[i], &bt[i]);
     }
 
-    calculate_SJF_Preemptive(p, n);
-    display(p, n);
+    findAvgTime(n, at, bt);
 
     return 0;
 }
